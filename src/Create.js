1955ada043c1db49851918d1f2b0343088ef238e
Soham Nandi <alwayssoham17@gmail.com>
2021-08-07 14:48:58 +0530

    deploy react application to github pages

---
+++ src/Create.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+      history.push("/");
+    });
+  };
+
+  return (
+    <div className="create">
+      <h2>Add a new Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  // //removing because we will now delete from the db.json through API calls (GET, Delete etc)
+  // const handleDelete = (id) => {
+  //     const newBlogs = blogs.filter(blog => blog.id !== id)
+  //     setBlogs(newBlogs)
+
+  // }
+
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+
+  return (
+    <div className="home">
+      {error && <div>{error} </div>}
+      {isPending && <div>Loading... </div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs!" />}
+      {/* <BlogList blogs = { blogs.filter((blog) => blog.author === 'mario') }  title = "Mario's blogs" />        */}
+    </div>
+  );
+};
+
+export default Home;
